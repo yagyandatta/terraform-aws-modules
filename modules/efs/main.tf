e1876140daa23b2ff00eadd6e5f92dec709d56e6
yagyandatta <yagyandattamurmu@gmail.com>
2025-07-18 15:45:57 +0530

    modules: changed the provider version to the efs main.tf

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -5,12 +5,10 @@
 
 # required Version
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
